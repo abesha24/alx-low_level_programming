d6962e5f09d0c807c13a0da9402fa4dc15a27317
abesha24 <abesha24@gmail.com>
2023-02-16 09:32:31 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 /* this is for printing with \ in string */
-
 int main(void)
 {
-/* this is main function */
 
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
