6c6f0273bbc2a208762c49e04a18614d34da6605
abesha24 <abesha24@gmail.com>
2023-02-16 09:02:54 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
-*main - function begins and using puts function
-*
-* Return: function ends
-*/
-
+* main- prints "Programming is like building a multilingual puzzle",
+* Return: 0 
+* */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
