29cd0119ccea2d00541759de06c3ca4c17ef6606
abesha24 <abesha24@gmail.com>
2023-02-16 09:55:39 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-#!/bin/bash
-#include <stdio.h>
-
+#include<stdio.h>
 /**
-* main - Function begins
-*
-* Return: Function ends
+* main- prints the phrase with proper grammar
+* Return: 0 if properly exited always
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
+
