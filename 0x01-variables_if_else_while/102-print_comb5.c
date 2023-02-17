35e41d551be00d20006ab470557cefc1d52e50e5
abesha24 <abesha24@gmail.com>
2023-02-17 15:48:14 -0800

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Description:Write a program that prints all possible different combinations
+* two-digit numbers.
+*
+* Return: Always 0 (Success
+*/
+int main(void)
+{
+	int first, second;
+
+	for (first = 0; first <= 98; first++)
+	{
+		for (second = first + 1; second <= 99; second++)
+		{
+			putchar((first / 10) + '0');
+			putchar((first % 10) + '0');
+			putchar(' ');
+			putchar((second / 10) + '0');
+			putchar((second % 10) + '0');
+
+			if (first == 98 && second == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
