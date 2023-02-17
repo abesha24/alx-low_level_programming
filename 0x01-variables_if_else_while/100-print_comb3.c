62d563c6d6d63bff13ddfe2366e21263c418418c
abesha24 <abesha24@gmail.com>
2023-02-17 15:28:23 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,23 +2,30 @@
 /**
 * main - Entry point
 *
-* Description:Write a program that prints all possible combinations of
-* single-digit numbers.
+* Description:Write a program that prints all possible different combinations
+* two-digit numbers.
 *
 * Return: Always 0 (Success)
 */
 int main(void)
-{	int ch;
+{
+	int d1, d2;
 
-	/* your code goes there */
-	for (ch = 0 ; ch <= 9 ; ch++)
+	for (d1 = 0; d1 < 9; d1++)
 	{
-		putchar((ch % 10) + '0');
-		if (ch == 9)
-			continue;
-		putchar(',');
-		putchar(' ');
+		for (d2 = d1 + 1; d2 < 10; d2++)
+		{
+			putchar((d1 % 10) + '0');
+			putchar((d2 % 10) + '0');
+
+			if (d1 == 8 && d2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
 	}
+
 	putchar('\n');
-return (0);
+
+	return (0);
 }
