d820e306b4d0694ea54e968be23406ae23aa47c7
abesha24 <abesha24@gmail.com>
2023-02-27 18:32:38 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half-  prints every other character of a string,
+ * starting with the first character,
+ * followed by a new line.
+ * @str: string to print the characters
+ */
+
+void puts_half(char *str)
+{
+	int h, n, k;
+
+	h = strlen(str);
+	if (h % 2 == 0)
+	{
+		n = h / 2;
+	}
+	else
+	{
+		n = ((h / 2) + 1);
+	}
+
+	k = n;
+	while (k < h)
+	{
+		_putchar(str[k]);
+		k++;
+	}
+
+	_putchar('\n');
+
+}
