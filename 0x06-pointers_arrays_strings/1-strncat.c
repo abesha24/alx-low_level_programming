70b6ed784f7ee994f9e133c54604f145a8d793ac
abesha24 <abesha24@gmail.com>
2023-03-01 12:52:43 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main- checks the code
+ * main - check the code for me.
  *
  * Return: Always 0.
  */
@@ -16,13 +16,12 @@ int main(void)
 	printf("%s\n", s1);
 	printf("%s", s2);
 	ptr = _strncat(s1, s2, 1);
-	printf("%s", s1);
+	printf("%s\n", s1);
 	printf("%s", s2);
-	printf("%s", ptr);
+	printf("%s\n", ptr);
 	ptr = _strncat(s1, s2, 1024);
 	printf("%s", s1);
 	printf("%s", s2);
 	printf("%s", ptr);
-
 	return (0);
 }
