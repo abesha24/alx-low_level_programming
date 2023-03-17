063cd7109db7560b5c64ab75701ebe209ea186fd
abesha24 <abesha24@gmail.com>
2023-03-16 19:02:20 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,6 @@ int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
-
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
