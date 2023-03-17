fb228fa69c84a093ce17cdfe45f6228528302f66
abesha24 <abesha24@gmail.com>
2023-03-16 19:08:16 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,6 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 
 #endif
