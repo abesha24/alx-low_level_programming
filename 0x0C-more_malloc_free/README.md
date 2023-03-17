5dd12b9948e81bd6b69aad3bc8f601665882b50a
abesha24 <abesha24@gmail.com>
2023-03-16 18:55:44 -0700

    0x0C

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,3 @@
+0. Trust no one
+mandatory
+Write a function that allocates memory using malloc.
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+
+
+#endif
