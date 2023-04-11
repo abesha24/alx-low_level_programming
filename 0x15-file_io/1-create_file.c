6c306545e81883fc3a7137c0fa75a8ae1961e46e
abesha24 <abesha24@gmail.com>
2023-04-10 19:01:39 -0700

    update again

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,14 +15,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL) 
+	if (text_content != NULL) /* get the string length */
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (!text_content) 
+	if (!text_content) /* if no text to write, just create the file */
 	{
 		close(o);
 		return (1);
